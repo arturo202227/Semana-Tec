ff6f680f28f239601c3fca8108d67c1b43104345
arturo202227 <A01747417@itesm.mx>
2024-03-21 09:53:08 -0600

    Probabilidad de cambio posición
    Se agregaron las lineas para poner la probabilidad de cambio de posición de la comida en cada paso de un 10%

--- snake.py
+++ snake.py
@@ -31,12 +31,11 @@ def inside(head):
     return -200 < head.x < 190 and -200 < head.y < 190
 def move():
     """Move snake and food forward one segment."""
-    global food  # Use global keyword to modify the food variable
+    global food  # Use global keyword to modify food variable
 
     head = snake[-1].copy()
     head.move(aim)
 
-    # Check if the snake hits the boundaries or collides with itself
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
@@ -44,27 +43,19 @@ def move():
 
     snake.append(head)
 
-    # Check if the snake has eaten the food
     if head == food:
         print('Snake:', len(snake))
-        # Generate new random coordinates for the food
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
     else:
         snake.pop(0)
 
-    # Move the food randomly
-    direction = randrange(4)  # 0: right, 1: left, 2: up, 3: down
-    if direction == 0:
-        food.x += 10
-    elif direction == 1:
-        food.x -= 10
-    elif direction == 2:
-        food.y += 10
-    else:
-        food.y -= 10
+    # Move food with 1/10 probability
+    if randrange(10) == 0:
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
 
-    # Check if the food is inside the boundaries, adjust if necessary
+    # Check if food is inside boundaries, adjust if necessary
     if not inside(food):
         if food.x < -200:
             food.x = -200
@@ -75,21 +66,18 @@ def move():
         elif food.y > 190:
             food.y = 190
 
-    # Check if the snake has eaten food and make it grow
+    # Check if snake has eaten food
     if head == food:
         print('Snake:', len(snake))
-        # Add a new segment to the snake
-        snake.append(snake[-1].copy())
+        snake.append(snake[-1].copy())  # Add new section to the snake
 
     clear()
 
-    # Draw the snake and the food
     for body in snake:
         square(body.x, body.y, 9, 'black')
 
     square(food.x, food.y, 9, 'green')
     update()
-    # Call the move() function again after 100 milliseconds
     ontimer(move, 100)
 
 
